ea2b422d6505c61c675140d513e9846189194022
Angelica Vargas <55171977+angyvanne@users.noreply.github.com>
2022-11-17 17:24:34 -0600

    Update z-autoRpt.js

--- plugins/z-autoRpt.js
+++ plugins/z-autoRpt.js
@@ -43,7 +43,7 @@ m.limit = 0
 if (rtps1.includes(".Safari") || rtps1.includes(".safari")){
 m.reply(`🤖*hola, en un momento te comparto la informacion de los proximos safaris zone:*`)
 setTimeout( () => {
-m.reply(`*Lugar:* Gardens by the Bay\n*fecha:* desde el viernes, 18 de noviembre de 2022 hasta el domingo, 20 de noviembre\n*costos aproximados:* 238 pesos de 12:00 a 18:00 horas o 350 pesos de 10:00 a 18:00 horas\n*Cordenadas, por si eres un sucio fly:* Se subiran unos dias antes del evento* 😎🤝`)
+m.reply(`*Lugar:* Gardens by the Bay\n*fecha:* desde el viernes, 18 de noviembre de 2022 hasta el domingo, 20 de noviembre\n*costos aproximados:* 238 pesos de 12:00 a 18:00 horas o 350 pesos de 10:00 a 18:00 horas\n*Cordenadas, por si eres un sucio fly:* 1.281766732, 103.863613* 😎🤝`)
 }, 2500)
 m.limit = 0
 }
